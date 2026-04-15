b6bab47b41d63968b7adb35bf0c9cef297f048a0
Aqib Suhail <59912182+geeqib23@users.noreply.github.com>
2026-04-16 01:40:44 +0530

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> dp;
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        dp.assign(n, -1);
+        return dfs(0, nums);
+    }
+    bool dfs(int i,vector<int> &nums){
+        if(i == nums.size()-1) return true;
+        if(dp[i] != -1) return dp[i] == 1 ? true : false;
+        bool res = false;
+        for(int j = i+1;j<=i+nums[i] && j<nums.size();j++){
+            res = res || dfs(j,nums);
+            if(res == true) break;
+        }
+        dp[i] = res == true ? 1 : 0;
+        return res;
+    }
+};
