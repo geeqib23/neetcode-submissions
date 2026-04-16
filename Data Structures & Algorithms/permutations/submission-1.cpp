dd8cf7092e7477852e09af13cb1a5509997df726
Aqib Suhail <59912182+geeqib23@users.noreply.github.com>
2026-04-17 02:09:38 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<vector<int>> permute(vector<int>& nums) {
+        backtrack(0,vector<int>(nums.size(),INT_MAX),nums);
+        return res;
+    }
+    void backtrack(int i,vector<int> temp,vector<int> &nums){
+        if(i == nums.size())
+            res.push_back(temp);
+        for(int j = 0;j<nums.size();j++){
+            if(temp[j] == INT_MAX){
+                temp[j] = nums[i]; 
+                backtrack(i+1,temp,nums);
+                temp[j] = INT_MAX;
+            }
+        }
+    }
+};
