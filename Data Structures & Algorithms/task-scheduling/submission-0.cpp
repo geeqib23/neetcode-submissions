3a279e8c2a7986a27bd2cc3abf0d43fca0f1c42a
Aqib Suhail <59912182+geeqib23@users.noreply.github.com>
2026-04-23 22:02:32 +0530

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        priority_queue<pair<int,char>> maxHeap;
+        unordered_map<char,int> freq;
+        for(auto t:tasks){
+            if(!freq.count(t))
+                freq[t] = 1;
+            else
+                freq[t]++;
+        }
+        for(auto &[ch,f]:freq)
+            maxHeap.push({f,ch});
+        queue<tuple<int,char,int>> q;
+        int i = 0;
+        while(!maxHeap.empty() || !q.empty()){
+            if(!q.empty()){
+                auto [f,ch,ind] = q.front();
+                if(i >= ind){
+                    maxHeap.push({f,ch});
+                    q.pop();
+                }
+            }
+            if(!maxHeap.empty()){
+                auto [f,ch] = maxHeap.top();
+                // cout << ch << " ";
+                maxHeap.pop();
+                if(f-1>0)
+                    q.push({f-1,ch,i+n+1});
+            }
+            i++;
+        }
+        return i;
+    }
+};
