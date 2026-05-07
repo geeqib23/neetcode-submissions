8c50325b54ef11da1d077948283e59f4730fac9b
Aqib Suhail <59912182+geeqib23@users.noreply.github.com>
2026-05-07 23:19:38 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,27 @@
+class MinStack {
+public:
+    vector<int> prefix; //can use stack for better efficiency
+    stack<int> st;
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        st.push(val);
+        int mn = prefix.empty() ? val : min(prefix.back(),val);
+        prefix.push_back(mn);
+    }
+    
+    void pop() {
+        st.pop();
+        prefix.pop_back();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return prefix.back();
+    }
+};
