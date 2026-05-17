fbaf5facb203159573eb9585a30637ff1b391ce5
Aqib Suhail <59912182+geeqib23@users.noreply.github.com>
2026-05-17 16:40:56 +0530

    Add: count-paths - submission-5

---
+++ Data Structures & Algorithms/count-paths/submission-5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+//Math soluton(O(min(m,n))- out of m+n-2 steps, choose how many would be right(or down)
+    int uniquePaths(int m, int n) {
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+        if (m < n) { // calculating 6C2 is fsater than calculationg 6C4
+            swap(m, n);
+        }
+
+        long long res = 1;
+        int j = 1;
+        for (int i = m; i <= m + n - 2; i++) { //calculating m+n-2Cn-1
+            res *= i;
+            res /= j;
+            j++; // res = (m+n-2)(m+n-3)....m/(n-1)!
+        }
+
+        return res;
+    }
+};
