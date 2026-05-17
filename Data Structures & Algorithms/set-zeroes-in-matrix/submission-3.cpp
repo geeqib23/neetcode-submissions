d765c6d0ea2dbb3bc09fe08a9c9a637bd5404aee
Aqib Suhail <59912182+geeqib23@users.noreply.github.com>
2026-05-17 18:37:33 +0530

    Add: set-zeroes-in-matrix - submission-3

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-3.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+//Modifying the Matrix While Scanning
+//Forgetting to Track the First Row Separately
+//Incorrect Order of Final Updates
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size(),m = matrix[0].size();
+        bool col1 =false, row1 = false;
+        for(int i = 0;i<n;i++){
+            if(matrix[i][0] == 0)
+                col1 = true;
+        }
+        for(int i = 0;i<m;i++){
+            if(matrix[0][i] == 0)
+                row1 = true;
+        }
+        for(int i = 1;i<n;i++){
+            for(int j = 1;j<m;j++){
+                    if(matrix[i][j] == 0){
+                        matrix[0][j] = 0;
+                        matrix[i][0] = 0;
+                    }
+                }
+            }
+        for(int i = 1;i<n;i++){
+            for(int j = 1;j<m;j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+        }
+        if(col1){
+            for(int i = 0;i<n;i++)
+                matrix[i][0] = 0;
+        }
+        if(row1){
+            for(int i = 0;i<m;i++)
+                matrix[0][i] = 0;
+        }
+    }
+};
