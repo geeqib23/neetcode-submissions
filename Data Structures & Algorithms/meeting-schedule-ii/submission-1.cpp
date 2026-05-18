2a4e640aee01f8f4abad4bb4feb7a9b8be216b34
Aqib Suhail <59912182+geeqib23@users.noreply.github.com>
2026-05-18 20:12:09 +0530

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+// sweeping line algo
+    int minMeetingRooms(vector<Interval>& intervals) {
+        map<int, int> mp;
+        for (auto& i : intervals) {
+            mp[i.start]++;
+            mp[i.end]--;
+        }
+        int prev = 0, res = 0;
+        for (auto& [key, value] : mp) {
+            prev += value;
+            res = max(res, prev);
+        }
+        return res;
+    }
+};
