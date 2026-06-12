6f630cb91ab45919ca4a60aa2865b3d41e8bedd8
Aqib Suhail <59912182+geeqib23@users.noreply.github.com>
2026-06-12 12:55:44 +0530

    Add: regular-expression-matching - submission-5

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-5.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+//DONT USE LOOPS. Let j pointer be constant - for cleaner code
+// also .* can also mean xya not just xyy 
+    bool isMatch(string s, string p) {
+        int m = s.size(), n = p.size();
+        return dfs(0, 0, s, p, m, n);
+    }
+
+    bool dfs(int i, int j, const string& s, const string& p, int m, int n) {
+        if (j == n) return i == m;
+
+        bool match = (i < m && (s[i] == p[j] || p[j] == '.'));
+        if (j + 1 < n && p[j + 1] == '*') {
+            return dfs(i, j + 2, s, p, m, n) ||
+                   (match && dfs(i + 1, j, s, p, m, n));
+        }
+
+        if (match) {
+            return dfs(i + 1, j + 1, s, p, m, n);
+        }
+
+        return false;
+    }
+};
